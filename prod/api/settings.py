a07c6417938cf16e8a0aaf0b6bffd345b9e5a554
Atharva Lele <xlordatharva@gmail.com>
2024-04-23 14:58:46 +0100

    Created API Endpoint to get User Settings by UserID and to update User Settings

---
+++ prod/api/settings.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+from db import settings
+
+app = Flask(__name__)
+api = Api(app)
+
+# Endpoint to get user settings by user ID
+class UserSettings(Resource):
+    def get(self, uid):
+        settings_data = settings.get_settings(uid)
+        if settings_data:
+            settings_dict = {
+                'uid': settings_data[0],
+                'calorie_compensation': settings_data[1],
+                'protein_goal': settings_data[2],
+                'display_calories': settings_data[3],
+                'display_protein': settings_data[4],
+                'display_fat': settings_data[5],
+                'display_carbs': settings_data[6]
+            }
+            return jsonify(settings_dict)
+        else:
+            return jsonify({'message': 'User settings not found'}), 404
+
+# Endpoint to update user settings
+class UpdateUserSettings(Resource):
+    def put(self, uid):
+        # Get the updated settings from the request body
+        updated_settings = request.json
+        # Update the settings in the database
+        settings.update_settings(uid, **updated_settings)
+        return jsonify({'message': 'User settings updated successfully'})
+
+api.add_resource(UserSettings, '/api/settings/<int:uid>')
+api.add_resource(UpdateUserSettings, '/api/settings/<int:uid>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
