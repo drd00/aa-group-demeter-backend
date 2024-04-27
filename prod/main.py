64bb7b568e3b4cf667541ab20997679192500310
drd00 <dohertd3@tcd.ie>
2024-04-27 14:07:58 +0100

    Settings now working perfectly with frontend

--- prod/main.py
+++ prod/main.py
@@ -8,8 +8,7 @@ from api.security.firebase import initialise_firebase
 from api.profile import Profile 
 from api.searchfood import SearchFood
 from api.diary import Diary
-from api.settings import UserSettings, UpdateUserSettings
-from api.default_settings import default_settings  # Import default settings
+from api.settings import Settings
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path, override=True)
@@ -27,11 +26,7 @@ CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}}, method
 api.add_resource(SearchFood, '/searchfood/<string:food_name>')
 api.add_resource(Profile, '/profile')
 api.add_resource(Diary, '/diary')
-api.add_resource(UserSettings, '/settings')
-# Endpoint to get default settings
-@app.route('/api/default-settings')
-def get_default_settings():
-    return jsonify(default_settings)
+api.add_resource(Settings, '/settings')
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
